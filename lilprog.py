4fa0a672df0866bbe2b3c4d905c08aabe0cfd04c
A1ZEKsSsS <blizya727@mail.ru>
2021-03-25 01:20:06 +0200

    Extended code

--- lilprog.py
+++ lilprog.py
@@ -1,2 +1,4 @@
-a=input()
-print('Hello wrold form Danya:)'+a)
+First_name=input(" What's your name? ")
+Question=input(' Who r u? ')
+space=(' ')
+print(' Hello world form'+First_name+"."+space+Question)
