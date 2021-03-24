1d9aa4bd2c1915052e12ce4df6e3d929ba9a78da
A1ZEKsSsS <blizya727@mail.ru>
2021-03-25 01:24:59 +0200

    Comment added

--- lilprog.py
+++ lilprog.py
@@ -1,4 +1,4 @@
 First_name=input(" What's your name? ")
 Question=input(' Who r u? ')
 space=(' ')
-print(' Hello world form'+First_name+"."+space+Question)
+print(' Hello world form'+First_name+"."+space+Question+"Коментар")
